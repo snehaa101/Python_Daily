f781ec23a3987103f6512ed2a7adcdb6207aec08
Sneha Dubey <snehadubey1911@gmail.com>
2025-11-18 14:34:47 +0530

    update new question

--- conditions/if_else.py
+++ conditions/if_else.py
@@ -14,26 +14,26 @@
 # ***EXAMPLES***
 
 ## VOTING AGE
-# a=int(input("Enter your age : "))
-# print ("Your age is ",a)
+a=int(input("Enter your age : "))
+print ("Your age is ",a)
 
-#    # conditional operator [==,<,>,<=,>=.!=]
-# if (a>=18):
-#     print(" You can drive")
-# else:
-#     print("You cannot drive")
+   # conditional operator [==,<,>,<=,>=.!=]
+if (a>=18):
+    print(" You can drive")
+else:
+    print("You cannot drive")
 
-# #--------------------------------------------
-# ## LARGEST NUMBER
-# b=int(input("Enter first number :"))
-# c=int(input("Enter second number :"))
+#-----------------------------------------------------
+## LARGEST NUMBER
+b=int(input("Enter first number :"))
+c=int(input("Enter second number :"))
 
-# if (b>c):
-#     print(b," is greater")
-# else:
-#     print(c," is greater")
+if (b>c):
+    print(b," is greater")
+else:
+    print(c," is greater")
 
-#---------------------------------------------
+#----------------------------------------------------
 ## PRINT LAST DIGIT and IF DIV BY 5
 num = int(input("Enter the number : "))
 
