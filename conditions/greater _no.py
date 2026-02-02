ace6a1da6828b0dc614a450329b5067fb9dcbe1d
snehaa101 <snehadubey517@gmail.com>
2026-02-03 00:26:00 +0530

    added basic file

--- conditions/greater _no.py
+++
@@ -1,9 +0,0 @@
-a = int(input("enter the first number : "))
-b = int(input("enter the second number : "))
-
-if a>b:
-    print(f"{a} is greter than {b}")
-elif b>a:
-    print(f"{b} is greter than {a}")
-else:
-    print(f"both numbers are equal")
