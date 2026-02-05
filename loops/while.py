2864b8b4a644077a21d37c01613403cbd18f0f21
snehaa101 <snehadubey517@gmail.com>
2026-02-05 23:05:46 +0530

    added while loop file

---
+++ loops/while.py
@@ -0,0 +1,11 @@
+# SYNTAX
+        #   while(condition){
+        #     -------body-----
+        #   }
+
+#print hey 100 times
+counter= 0
+while counter<100:
+        print("heyy....!!!!")
+
+        counter += 1
