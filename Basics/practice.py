dd6863afe4ff7243b87567c4716c39150b9e336e
Sneha Dubey <snehadubey1911gmail.com>
2026-02-06 16:49:28 +0530

    added a practise file

---
+++ Basics/practice.py
@@ -0,0 +1,17 @@
+#comparison operator find (a=34)>(b=80)
+a=int(input("enter value of a :"))
+b=int(input("enter value of b :"))
+
+if (a>b):
+    print("a is greater", a)
+else:
+    print("b is greater", b)
+
+#AVG OF NUMBERS
+print("The avg of two number is", (a+b)/2)
+
+#SQUARE OF NUMBER 
+print("the square of a numbers are", (a*a),"and", (b*b))
+
+#CUBE OF NUMBER
+print("The cube of the numbers are",(a**3),"and", (b**3))
