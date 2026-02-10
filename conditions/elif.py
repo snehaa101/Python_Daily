ce0ac7a7254599045f21e792f64c16ef9113fa43
Sneha Dubey <snehadubey1911@gmail.com>
2026-02-10 19:03:31 +0530

    added new example

--- conditions/elif.py
+++ conditions/elif.py
@@ -54,3 +54,18 @@ elif percentage>=1 :
     print("Sorry you FAILED")
 else:
     print("INVALID") # percentage > 100
+
+
+
+#### P2 #### 
+# GREATER NUMBER 
+
+a = int(input("enter the first number : "))
+b = int(input("enter the second number : "))
+
+if a>b:
+    print(f"{a} is greter than {b}")
+elif b>a:
+    print(f"{b} is greter than {a}")
+else:
+    print(f"both numbers are equal")
