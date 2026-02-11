fff30ba1b461c49cd4776f010ab23699d9da9b89
Sneha Dubey <snehadubey1911@gmail.com>
2026-02-11 11:50:06 +0530

    added questions

---
+++ conditions/even-odd.py
@@ -0,0 +1,8 @@
+# Accept an integer and check whether it is an EVEN or an ODD number
+
+a = int(input("enter the number : "))
+
+if a%2==0 :
+    print (f"{a} is an even number")
+else:
+    print (f"{a} is an odd number")
