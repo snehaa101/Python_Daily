f7fe73a2a0b54f74aab47264aa0fea36de2877cd
Sneha Dubey <snehadubey1911@gmail.com>
2026-02-15 23:44:53 +0530

    added loops

---
+++ loops/for loop/for.py
@@ -0,0 +1,12 @@
+###-----IMP POINTS----###
+
+#1# A for loop is used for iterating over a sequence 
+#    (that is either a list, a tuple, a dictionary, a set, or a string).
+
+#2# With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+
+#3# range(start, stop, step)
+
+#4# SYNTAX
+#         for variable in sequence:
+    # block of code to be executed
--- loops/while.py
+++ loops/while.py
@@ -1,3 +1,5 @@
+## With the while loop we can execute a set of statements 
+#   as long as a condition is true.
 # SYNTAX
         #   while(condition){
         #     -------body-----
