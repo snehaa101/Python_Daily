4128d76ca40c2f414857b2919ccfe80486724203
Sneha Dubey <snehadubey1911@gmail.com>
2026-02-25 15:10:18 +0530

    edit solution

--- loops/for loop/table.py
+++ loops/for loop/table.py
@@ -6,8 +6,12 @@
 #taking the input from the user
 n = int(input("Enter the you want a table of: "))
 
+#loop for just printing the ans
 for i in range(1,11):
     num=n*i
-    print(f"{n}*{i}={num}")
+    print(f"{num}")
 
-    
+#loop for table in {2*1=2} form
+print("table in {2*1=2} form")
+for i in range(1,11):
+    print(f"{n}*{i}={n*i}")    
