13050c1971a9ab4822a1a65c79f0f3ec44052def
Sneha Dubey <snehadubey1911@gmail.com>
2026-02-28 23:01:04 +0530

    added while loop examples

---
+++ loops/while/list_using_while.py
@@ -0,0 +1,6 @@
+#--------------------------------------------------------------
+# Topic: PYTHON LOOPS (while loop)
+# Problem: WAP to print the content of a list using while loop
+# -------------------------------------------------------------
+
+list =["sneha","shanvi",517]
--- loops/while/while.py
+++ loops/while/while.py
@@ -1,13 +1,46 @@
-## With the while loop we can execute a set of statements 
-#   as long as a condition is true.
-# SYNTAX
+#-------------------------------------------------------
+# Topic : Python Loops (FOR LOOP)
+# Description :
+#          - Syntax
+#          - Simple examples of using basic FOR loop 
+#-------------------------------------------------------
+## With the while loop we can execute a set of statements as long as a condition is true.
+
+##Check condition
+# ↓
+# If True → Run loop body
+# ↓
+#increment to avoid infinite loop
+# ↓
+# Go back and check condition again
+# ↓
+# Repeat until condition becomes False
+
+## SYNTAX
         #   while(condition){
         #     -------body-----
         #   }
+#------------------------------------------------------
 
-#print hey 100 times
-counter= 0
+## EXAMPLE ##
+
+#-print hey 100 times
+
+counter= 0 # starting value
+
+#loop runs 100 times
 while counter<100:
         print("heyy....!!!!")
+        #increment to avoid infinite loop
+        counter += 1 
+#-------------------------------------------------
 
-        counter += 1
+#-print 1 to 10
+
+i=1 #initialzing
+
+#loop prints till 10
+while i<=10:
+        print(i)
+        #increment to avoid infinite loop
+        i+=1
