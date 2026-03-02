4640bf0610300077064b8fc4f4dcf104b498bb6b
Sneha Dubey <snehadubey1911@gmail.com>
2026-03-02 21:05:50 +0530

    added program to reverse a string using for loop

--- loops/for loop/factor_of_number.py
+++ loops/for loop/factor_of_number.py
@@ -3,3 +3,14 @@
 # Problem: WAP a program to find all the factor of a number
 #-------------------------------------------------------------------------
 
+## Factor of a number is a number which divides the given number completely without leaving any remainder.
+
+# Taking input from user
+n = int(input("Enter a number: "))
+
+# Using for loop to find factors of the number
+
+for i in range (1,n+1):
+    if n%i==0:
+        print(i)
+
