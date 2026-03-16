8bd85dc168cbd2cdf5445eb056f81b9ea2a033f8
Sneha Dubey <snehadubey1911@gmail.com>
2026-03-16 23:18:29 +0530

    added sample outputs

--- loops/for loop/sum_of_even&odd.py
+++ loops/for loop/sum_of_even&odd.py
@@ -1,16 +1,29 @@
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Topic: PYTHON LOOPS (for loop)
 # Problem: Print the sum of all even and odd number in a range separately
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 
-n=int(input("Enter a number:"))
+n = int(input("Enter a number:"))
 
-even=0
-odd=0
+even = 0
+odd = 0
 
-for i in range(1,n+1):
-    if i%2==0:
-        even +=i
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        even += i
     else:
-        odd+=i
-print(f"The sum of all odd numbers among {n} numbers is {odd}.\nand\nThe sum of all even numbers among {n} numbers is {even}")
+        odd += i
+print(
+    f"The sum of all odd numbers among {n} numbers is {odd}.\nand\nThe sum of all even numbers among {n} numbers is {even}"
+)
+
+
+"""
+SAMPLE OUTPUT : 
+
+Enter a number:4412
+The sum of all odd numbers among 4412 numbers is 4866436.
+and
+The sum of all even numbers among 4412 numbers is 4868642
+
+"""
